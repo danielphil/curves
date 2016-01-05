3ec28b27f3b2df5d725c2b2939bcb3766f5dccb7
Daniel Phillips <dphildev@gmail.com>
2016-01-05 15:26:16 +0000

    Experimenting with ways to replace the geometry on the fly.

--- src/CurveRenderView.ts
+++ src/CurveRenderView.ts
@@ -5,48 +5,17 @@ module Curves {
     export class CurveRenderView extends ThreeView
     {
         private curve = new Curves.Hermite();
+        private mesh: THREE.Mesh;
         
         constructor(container: HTMLElement) {
             super(container);
             
-            var leftX = -1.0;
-            var rightX = 1.0;
-            
+            // Create a dummy geometry for now
             var geometry = new THREE.BufferGeometry();
-			// create a simple square shape. We duplicate the top left and bottom right
-			// vertices because each vertex needs to appear once per triangle.
-			// three components per vertex
-			// components of the position vector for each vertex are stored
-			// contiguously in the buffer.
-			var vertices = new Float32Array([
-				leftX, -1.0, 0,
-                leftX, 1.0, 0,
-				rightX, -1.0, 0,
-                rightX, -1.0, 0,
-                leftX, 1.0, 0,
-				rightX, 1.0, 0
-			]);
-			var uv = new Float32Array([
-				0, 1,
-				1, 1,
-				1, 0,
-				1, 0,
-				0, 0,
-				0, 1
-			]);
-            var color = new Float32Array([
-                1, 0, 0,
-                0, 0, 1,
-                0, 1, 0,
-                0, 1, 0,
-                0, 0, 1,
-                1, 1, 1
-            ]);
-			// itemSize = 3 because there are 3 values (components) per vertex
-			geometry.addAttribute('position', new THREE.BufferAttribute( vertices, 3 ) );
-			geometry.addAttribute('uv', new THREE.BufferAttribute(uv, 2));
-            geometry.addAttribute('color', new THREE.BufferAttribute(color, 3));
-			
+			geometry.addAttribute('position', new THREE.BufferAttribute(new Float32Array([]), 3));           
+			geometry.addAttribute('uv', new THREE.BufferAttribute(new Float32Array([]), 2));           
+            geometry.addAttribute('color', new THREE.BufferAttribute(new Float32Array([]), 3));
+
 			var material = new THREE.MeshBasicMaterial( {
                 vertexColors: THREE.VertexColors,
                 wireframe: true,
@@ -54,8 +23,8 @@ module Curves {
                 depthWrite: false
             });
             
-			var cube = new THREE.Mesh( geometry, material );
-			this.scene.add(cube);
+			this.mesh = new THREE.Mesh(geometry, material );
+			this.scene.add(this.mesh);
             
             this.camera.position.z = 1.5;
 			this.camera.updateProjectionMatrix();
@@ -66,6 +35,49 @@ module Curves {
 
         setCurve(curve: Curves.Hermite) {
 			this.curve = curve;
+            
+            if (curve.points.length < 2) {
+                return;
+            }
+                        
+            var leftX = -1.0;
+            var rightX = 1.0;
+        
+            var vertices = new Float32Array([
+                leftX, -1.0, 0,
+                leftX, 1.0, 0,
+                rightX, -1.0, 0,
+                rightX, -1.0, 0,
+                leftX, 1.0, 0,
+                rightX, 1.0, 0
+            ]);
+            
+            var uv = new Float32Array([
+                0, 1,
+                1, 1,
+                1, 0,
+                1, 0,
+                0, 0,
+                0, 1
+            ]);
+            
+            var color = new Float32Array([
+                1, 0, 0,
+                0, 0, 1,
+                0, 1, 0,
+                0, 1, 0,
+                0, 0, 1,
+                1, 1, 1
+            ]);
+            
+            var geometry = new THREE.BufferGeometry();
+			geometry.addAttribute('position', new THREE.BufferAttribute(vertices, 3));           
+			geometry.addAttribute('uv', new THREE.BufferAttribute(uv, 2));           
+            geometry.addAttribute('color', new THREE.BufferAttribute(color, 3));
+            
+            this.mesh.geometry.dispose();
+            // BufferGeometry not convertable to Geometry? Hence cast below.
+            this.mesh.geometry = <any>geometry;
 		}
     }
 }
